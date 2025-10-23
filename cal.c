63bbf92e25426d6c5a9b39cb32d95137ac2b04a0
Kunal nande <nandekunal03@gmail.com>
2025-10-23 08:27:35 +0530

    Correct input handling and prompt in calculator
    Fixed input format for numbers and added prompt for user input.

--- cal.c
+++ cal.c
@@ -5,7 +5,9 @@ void main()
  char c;
  printf("\n+ Add, - substaract, x Multiplay, / Devide"); 
  printf("\nSelect your choice");
- scanf("&c",%c);
+ scanf("%c",&c);
+ printf("\n Enter any tow number");
+ scanf("%d,%d",&a,&b);
  if(c=='+')
    res=a+b;
  else if(c=='-')
@@ -17,4 +19,5 @@ void main()
  else
   printf("\n Select your Choice");
  printf("\n Result = %d",res);
+
 }
